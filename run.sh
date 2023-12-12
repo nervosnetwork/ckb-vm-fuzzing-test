cb5252beb1ab69d196ea4e3ee9ebfd6163baed48
mohanson <mohanson@outlook.com>
2023-12-12 15:45:08 +0800

    Add old corpus

--- run.sh
+++ run.sh
@@ -2,19 +2,23 @@
 
 BRANCH="${1:-develop}"
 
-mkdir -p deps/ckb-vm-${BRANCH}/fuzz/corpus/asm
-mkdir -p deps/ckb-vm-${BRANCH}/fuzz/corpus/isa_a
-mkdir -p deps/ckb-vm-${BRANCH}/fuzz/corpus/isa_b
+cd deps
+rm -rf ckb-vm
+git clone https://github.com/nervosnetwork/ckb-vm --depth 1 --branch ${BRANCH}
+cd -
 
-cp corpus/asm/* deps/ckb-vm-${BRANCH}/fuzz/corpus/asm/
-cp corpus/isa_a/* deps/ckb-vm-${BRANCH}/fuzz/corpus/isa_a/
-cp corpus/isa_b/* deps/ckb-vm-${BRANCH}/fuzz/corpus/isa_b/
+mkdir -p deps/ckb-vm/fuzz/corpus/asm
+mkdir -p deps/ckb-vm/fuzz/corpus/isa_a
+mkdir -p deps/ckb-vm/fuzz/corpus/isa_b
 
-echo "nightly" >> deps/ckb-vm-${BRANCH}/rust-toolchain
-cd deps/ckb-vm-${BRANCH}
+cp corpus/asm/* deps/ckb-vm/fuzz/corpus/asm/
+cp corpus/isa_a/* deps/ckb-vm/fuzz/corpus/isa_a/
+cp corpus/isa_b/* deps/ckb-vm/fuzz/corpus/isa_b/
 
-cargo fuzz run -j 4 asm
-cargo fuzz run -j 4 isa_a
-cargo fuzz run -j 4 isa_b
+cd deps/ckb-vm
+
+cargo +nightly fuzz run -j 4 asm -- -max_total_time=57600 -timeout=2 -max_len=614400
+cargo +nightly fuzz run -j 4 isa_a -- -max_total_time=14400 -timeout=2 -max_len=614400
+cargo +nightly fuzz run -j 4 isa_b -- -max_total_time=14400 -timeout=2 -max_len=614400
 
 cd -

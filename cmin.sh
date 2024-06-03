e2c6e21da80d8875c36d77d6211a94374db42904
Mohanson <mohanson@outlook.com>
2024-06-03 15:40:06 +0800

    Update cmin.sh (#11)

--- cmin.sh
+++ cmin.sh
@@ -1,10 +1,8 @@
-
-
 cd deps/ckb-vm/fuzz
 cargo +nightly fuzz cmin asm
 cargo +nightly fuzz cmin interpreter
 cargo +nightly fuzz cmin isa_a
 cargo +nightly fuzz cmin isa_b
+cargo +nightly fuzz cmin snapshot
+cargo +nightly fuzz cmin snapshot2
 cd -
-
-
